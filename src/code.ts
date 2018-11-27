4433c6b93d43668bb3bb79db84afbca253eb2232
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-11-27 13:30:57 +0200

    some examples from https://dev.to/sagar/

--- src/code.ts
+++ src/code.ts
@@ -1,13 +1,40 @@
 
 import * as Rx from 'rxjs';
-
-console.log(Rx);
+import { Observable, Observer } from 'rxjs';
 
 const demoObservable = new Rx.Observable(observer => {
   setInterval(() => {
       observer.next('value');
-      observer.complete();
-    });
+      // observer.complete(); // with this 'complete' will log only one 'value'
+    }, 1000);
   });
-  demoObservable.subscribe(console.log); // value
-  
+const subscription = demoObservable.subscribe(console.log); // value
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 5000);
+
+// === SUBJECT ===
+const subject = new Rx.Subject();
+
+subject.subscribe({
+  next: v => console.log('ObserverA: ' + v)
+});
+subject.subscribe({
+  next: v => console.log('ObserverB: ' + v)
+});
+
+subject.next(1);
+subject.next(2);
+
+// Creating observables from values
+const obs = Rx.of(5, 'hello', [1, 'doe'], { id: 23 }, true);
+obs.subscribe(console.log);
+
+// Creating Observables from stream of values
+const obsStream = Observable.create(function(observer: Observer<string>) {
+  observer.next('Hello');
+  observer.next('How are you?');
+});
+
+obsStream.subscribe(console.log);
