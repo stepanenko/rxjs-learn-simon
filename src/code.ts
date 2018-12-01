df16f5e19afbaeca695d2760c2a02f335d2a098d
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-12-01 14:41:19 +0200

    one half of video done

--- src/code.ts
+++ src/code.ts
@@ -1,1 +1,8 @@
-console.log('Hello world');
+import { Observable, Subscription, fromEvent } from 'rxjs';
+
+function addItem(val: any) {
+  const node = document.createElement('li');
+  const textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById('output').appendChild(node);
+}
---
+++ src/first-half-gary.ts
@@ -0,0 +1,34 @@
+import { Observable, Subscription } from 'rxjs';
+import { share } from 'rxjs/operators';
+
+// const observable = Observable.create(function subscribe(observer: any) {
+const observable: Observable<any> = Observable.create((observer: any) => {
+  try {
+    observer.next('Hey guys!');
+    observer.next('How are you?');
+    setInterval(() => {
+      observer.next('I am good');
+    }, 2000)
+  } catch(err) {
+    observer.error('Error: ', err);
+  }
+}).pipe(share());
+
+const observer: Subscription = observable.subscribe(
+  (x: any) => addItem(x),
+  (error: any) => addItem(error),
+  () => addItem('completed')
+);
+
+setTimeout(() => {
+  const observer2 = observable.subscribe(
+    (x: any) => addItem('Subscriber 2: ' + x)
+  )
+}, 1000);
+
+function addItem(val: any) {
+  const node = document.createElement('li');
+  const textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById('output').appendChild(node);
+}
